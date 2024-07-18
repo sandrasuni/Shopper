4c4dcc7f3dad26f0f605a914838d127713a738e3
sandrasuni <147145698+sandrasuni@users.noreply.github.com>
2024-07-18 22:29:57 +0530

    Add files via upload

---
+++ backend/assets/all_product.js
@@ -0,0 +1,127 @@
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from "./img3.jpg"
+import img4 from "./img4.jpg"
+import img16 from "./img16.jpg"
+import img9 from './img9.jpg'
+import img10 from "./img10.jpg"
+import img11 from "./img11.jpg"
+import img12 from "./img12.jpg"
+import img13 from "./img13.jpg"
+import img14 from "./img14.jpg"
+import img15 from "./img15.jpg"
+
+
+
+ let all_product = [
+    {
+        id:1,
+        name:"Nightfall flora",
+        category:"women",
+        // desc:"A graceful black & multicolour floral hand embroidery dress featuring round neck, puffed sleeves, fitted waist, gathered hem and button detailing at the front.",
+        new_price:20,
+        old_price:30,
+        image:img1,
+        
+    },
+        
+    {
+        id:2,
+        name:"Blushing pearl",
+        category:"women",
+        // desc:"A graceful black & multicolour floral hand embroidery dress featuring round neck puffed sleeves, fitted waist, gathered hem and button detailing at the front.",
+        new_price: 20,
+        old_price:30,
+        image:img2,
+    },
+        
+    {
+        id:3,
+        name:"Elegant enigma",
+        category:"women",
+        // desc:"A classy ethnic ensemble featuring a black kurthi in ethnic prints and stripes with a shirt collar on a V-neck and button-up flaps on the sleeve end .",
+        new_price: 90,
+        old_price:100,
+        image:img3,
+    },
+    {
+        id:4,
+        name:" Celio",
+        category:"men",
+        // desc:"Dl Fashion Men Self Design Casual  Shirt with beautiful neck with pant and get stylish good neat awsome looks.diffrent colors are also available.",
+        new_price: 10,
+        old_price:20,
+        image:img4,
+    },
+    {
+        id:5,
+        name:"White shirt",
+        category:"men",
+        // desc:"A classy ethnic ensemble featuring a black kurthi in ethnic prints and stripes with a shirt collar on a V-neck and button-up flaps on the sleeve end paired with chic pencil.",
+        new_price: 30,
+        old_price:40,
+        image:img16,
+    },
+    {
+        id:6,
+        name:"Three-quarter Sleeve ",
+        category:"women",
+        // desc:"dresses women for dress dress party wear for women latest dress get stylish good neat awsome looks.diffrent colors are also available.",
+        new_price: 15,
+        old_price:20,
+        image:img9,
+    },
+    {
+        id:7,
+        name:"Single Breasted ",
+        category:"men",
+        // desc:"Peter England Men Single Breasted - 2 button Solid Formal Two Piece Suit.get stylish good neat awsome looks.diffrent colors are also available.",
+        new_price: 16,
+        old_price:25,
+        image:img10,
+    },
+    {
+        id:8,
+        name:"Plain Formal Shirt ",
+        category:"men",
+        // desc:"Plain Formal Shirt Trouser Combo For Men.get stylish good neat awsome looks.diffrent colors are also available.button placket, no pocket, long regular sleeve",
+        new_price: 10,
+        old_price:20,
+        image:img11,
+    },
+    {
+        id:9,
+        name:"Locomotive Premium Men ",
+        category:"men",
+        // desc:"Olive green self design opaque party shirt ,has a spread collar, button placket, no pocket, long regular sleeve with stylish awsome looks",
+        new_price: 40,
+        old_price:20,
+        image:img12,
+    },
+    {
+        id:10,
+        name:"Casual T-shirt Pant",
+        // desc:"Primary Color Is Multicolor. Brand Color Is Grey, Black. Label Size Is M. Primary Product Type Is T-shirt. Occasion Is Casual. Ideal For Is Boys",
+        new_price: 10,
+        old_price:20,
+        image:img13,
+    },
+    {
+        id:11,
+        name:"Printed T-shirt ",
+        // desc:"The Souled Store Girls Typography Printed T-shirt (2-3Y).button placket, no pocket, long regular sleevebutton placket, no pocket, long regular sleeve",
+        new_price: 10,
+        old_price:20,
+        image:img14,
+    },
+    {
+        id:12,
+        name:"Printed Boys Shirt",
+        category:"kid",
+        // desc:"Double Fun Printed Boys Shirt.button placket, no pocket, long regular sleeve.button placket, no pocket, long regular sleeve.button placket, no pocket, long regular sleeve",
+        new_price: 9,
+        old_price:15,
+        image:img15,
+    },
+];
+export default all_product;
